04486c07538bea9dab42a812459e6646fe1e1b27
GuiDias123 <164422098+GuiDias123@users.noreply.github.com>
2024-05-28 19:05:43 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroAcertos = 0;
+        System.out.println("===== Quiz: Geografia =====");
+        numeroAcertos += grupo2();
+        System.out.println("Final do Quiz! Parabéns... De 15 perguntas, você acertou: " + numeroAcertos);
+    }
+
+    public static int grupo2() {
+        int acertosGrupo2 = 3;
+        return acertosGrupo2;
+    }
+}
